25bdbe29a866bc3ae927e934dace480e5fcd393e
Kali Madina <kmkstud@gmail.com>
2021-05-08 22:14:49 +0300

    initial

--- ft_split.c
+++ ft_split.c
@@ -1,13 +1,73 @@
-char	**ft_split(char const *s, char c)
+#include <stdlib.h>
+#include "libft.h"
+
+static int	c_w(char const *s, char c)
 {
-	int		i;
-	char	**m;
+	int i;
+	int count;
 
-	i = 0;
-	while (s[i])
+	i = -1;
+	count = 1;
+	if (!s[0])
+		return (0);
+	while (s[++i])
 	{
-		if (s[i] == c)
-			get_word(s, i);
-
+		if (s[i] && s[i] == c && s[i + 1] != c)
+			count++;
 	}
+	if (s[0] == c)
+		count--;
+	if (s[i - 1] == c)
+		count--;
+	return (count);
+}
+
+static char	**freemem(char **array)
+{
+	int i;
+
+	i = -1;
+	while (array[++i])
+		free(array[i]);
+	free(array);
+	return (NULL);
+}
+
+static int	l_n_s(const char *s, char c)
+{
+	int i;
+	int len;
+
+	i = -1;
+	len = 0;
+	while (s[++i] && s[i] != c)
+		len++;
+	return (len);
+}
+
+char		**ft_split(char const *s, char c)
+{
+	char	**array;
+	int		i;
+	int		is_char;
+	int		j;
+
+	if (!s || !(array = (char**)malloc(sizeof(char*) * (c_w(s, c) + 1))))
+		return (NULL);
+	j = -1;
+	i = -1;
+	is_char = 1;
+	while (s[++i])
+	{
+		if (!is_char && s[i] == c && s[i + 1] != c && s[i + 1])
+			is_char = 1;
+		if (is_char && s[i] != c)
+		{
+			if (!(array[++j] = ft_substr(s + i, 0, l_n_s(s + i, c))))
+				return (freemem(array));
+			is_char = 0;
+		}
+	}
+	array[++j] = 0;
+	return (array);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -8,11 +8,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	i = 0;
 	s = 0;
-	while (in_set(s1[i++]))
+	while (in_set(set, s1[i++]))
 		s++;
 	l = ft_strlen(s1);
 	e = 0;
-	while (in_set(s1[--l]))
+	while (in_set(set, s1[--l]))
 		e++;
 	s2 = (char *)malloc(sizeof(char) * (ft_strlen(s1) - s - e));
 	if (!s2)
@@ -20,4 +20,12 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (s1[s])
 		*s2 = s1[s++];
 	return (s2);
+}
+
+int 	in_set(char *s, char c)
+{
+	while (*s)
+		if (*s++ == c)
+			return (1);
+	return (0);
 }
