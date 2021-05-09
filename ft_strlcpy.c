3a683b2b8a0c7777701f2ea2aa3b48cf012f3f5a
Kali Madina <kmkstud@gmail.com>
2021-05-09 22:43:58 +0300

    initial

---
+++ ft_strlcpy.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	if (!size)
+		return (ft_strlen(src));
+	while (src[i] && i + 1 < size)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
---
+++ ft_strlen.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int c;
+
+	c = 0;
+	while (s[c])
+		c++;
+	return (c);
+}
+
