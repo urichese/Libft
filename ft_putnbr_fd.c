f7e5a33da2623f8ca9e33b62c0c3936b85180807
Kali Madina <kmkstud@gmail.com>
2021-05-09 15:57:23 +0300

    initial

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int nb;
+
+	nb = n;
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nb = -nb;
+	}
+	if (nb / 10)
+		ft_putnbr_fd(nb / 10, fd);
+	ft_putchar_fd(nb % 10 + '0', fd);
+}
+
